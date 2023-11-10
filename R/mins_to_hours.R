6f199b11200eb9cb6d865f8324a9c83c735cfd05
danahalay <2000halay@gmail.com>
2023-11-09 22:37:29 -0800

    initial commit

---
+++ R/mins_to_hours.R
@@ -0,0 +1,8 @@
+#' @title mins_to_hours
+#' @description mins_to_hours returns the numeric value converted from a measurement in minutes to a measurement in hours
+#' @param mins is a numeric, atomic, or vector. I named it "mins" because it should be an input of a number of minutes which will be converted to a number of hours.
+#' @return a numeric of the value divided by 60
+#' @examples
+#' mins_to_hours(60)
+#' mins_to_hours(1:5)
+#' @export
